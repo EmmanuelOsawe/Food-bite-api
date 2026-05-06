6f6ab23e764355155aa1d95e416abde9828b66e8
Your Name <emmanuelosawe90@gmail.com>
2026-05-06 10:32:42 -0700

    chg mon conne name

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.DB_CONNECT)
   .then(() => {
     console.log("✅ MongoDB connected");
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
